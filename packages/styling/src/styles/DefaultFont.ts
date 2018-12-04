407e2b27bb2f93cdd6b6a0975403b2aa5bacff71
Natalie Ethell <naethell@microsoft.com>
2018-12-04 10:49:47 -0800

    update DefaultFontVariants

--- packages/styling/src/styles/DefaultFont.ts
+++ packages/styling/src/styles/DefaultFont.ts
@@ -1,6 +1,7 @@
 import { DefaultFontStyles } from './DefaultFontStyles';
-import { FontSizes, FontWeights } from './fonts';
+import { FontSizes, FontWeights, createFonts } from './fonts';
 import { IFontFamilies, IFontSizes, IFontWeights, IFontVariants, IFont } from '../interfaces/IFont';
+import { getLanguage } from '@uifabric/utilities';
 
 export const DefaultFontSizes: IFontSizes = {
   // 70: '',
@@ -34,47 +35,7 @@ export const DefaultFontWeights: IFontWeights = {
   bold: FontWeights.bold
 };
 
-// TODO: use the original DefaultFontStyles for these
-export const DefaultFontVariants: IFontVariants = {
-  default: {
-    family: 'default',
-    size: 'inherit'
-  },
-
-  caption: {
-    size: 'xSmall'
-  },
-
-  h1: {
-    size: 'mega',
-    weight: 'light'
-  },
-
-  h2: {
-    size: 'xxLarge',
-    weight: 'light'
-  },
-
-  h3: {
-    size: 'xLarge',
-    weight: 'light'
-  },
-
-  h4: {
-    size: 'large',
-    weight: 'light'
-  },
-
-  h5: {
-    size: 'mediumPlus',
-    weight: 'light'
-  },
-
-  link: {
-    color: 'link',
-    hoverColor: 'linkHovered'
-  }
-};
+export const DefaultFontVariants: IFontVariants = createFonts(getLanguage());
 
 export const DefaultFont: IFont = {
   families: DefaultFontFamilies,
